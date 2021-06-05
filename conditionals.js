d475bc39243cf5868eea64742978739f73a196a0
marcschul <marcschulm@gmail.com>
2021-06-05 19:19:44 +0000

    updated conditionals.js with more conditions

--- conditionals.js
+++ conditionals.js
@@ -1,11 +1,58 @@
-const temperature = 12;
+// // raining --------------------------------------------
+// const raining = true;
 
-if (temperature < 0) {
-  console.log("Make sure you pick out a scarf!");
-} else if (temperature < 15) {
-  console.log("Short sleeves won't cut it!");
-} else {
-  console.log("Short sleeves are fine.");
+// if (raining) {
+//   console.log("Don't forget your umbrella!");
+// }
+
+// // go outside -----------------------------------------
+// const raining = true;
+// const cold = false;
+
+// if (raining) {
+//   console.log("Don't forget your umbrella!");
+// }
+
+// if (cold) {
+//   console.log("Make sure you pick out a scarf!");
+// }
+
+// console.log("Now you're ready to go outside!");
+// cold --------------------------------------------------
+// const cold = false;
+
+// if (cold) {
+//   console.log("Make sure you pick out a scarf!");
+// } else {
+//   console.log("Short sleeves are fine.");
+// }
+
+// temperature -------------------------------------------
+// const temperature = 12;
+
+// if (temperature < 0) {
+//   console.log("Make sure you pick out a scarf!");
+// } else if (temperature < 15) {
+//   console.log("Short sleeves won't cut it!");
+// } else {
+//   console.log("Short sleeves are fine.");
+// }
+// console.log("Now you're ready to go outside!");
+
+// vote -----------------------------------------------------
+// const isCitizen = true;
+// const age = 26;
+
+// if (isCitizen && age > 18) {
+//   console.log("You are eligible to vote.");
+// }
+// extreme temperature --------------------------------------
+// const temperature = -50;
+// if (temperature < -40 || temperature > 40) {
+//   console.log("Maybe going outside isn't such a great idea…");
+// }
+// raining again -----------------------------------------------
+let raining = false;z
+if (!raining) {
+  console.log("Leave your umbrella at home!");
 }
-
-console.log("Now you're ready to go outside!");
